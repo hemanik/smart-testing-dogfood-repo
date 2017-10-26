b8ffd5c1303f0f552c6cd3a69a5bdab18ac1c8e6
hemanik <hemanikatyal@gmail.com>
2017-10-26 12:58:49 +0530

    new: configure skipITs as default property in pom.
    + org.jboss.arquillian.core.ITDummyTestCase

---
+++ core/impl-base/src/test/java/org/jboss/arquillian/core/ITDummyTestCase.java
@@ -0,0 +1,12 @@
+package org.jboss.arquillian.core;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+public class ITDummyTestCase {
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
